2c5e8ab6277125899e5645c06cee839c23843432
dev-saiful <saifulcseian@gmail.com>
2023-09-05 22:36:24 +0600

    upsert functionality added

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -4,8 +4,6 @@ const {parse} = require('csv-parse');
 const planets = require('./planets.mongo');
 
 
-const habitablePlanets = [];
-
 function isHabitablePlanet(planet)
 {
     return planet['koi_disposition'] === 'CONFIRMED'
@@ -24,11 +22,7 @@ function loadPlanetsData()
     .on('data',async (data)=>{
         if(isHabitablePlanet(data))
         {
-            // TODO : replace insert to upsert = insert+ update
-            // insert data
-            // await planets.create({
-            //     keplerName:data.kepler_name,
-            // });
+          savePlanet(data);
         }
         
     })
@@ -36,9 +30,9 @@ function loadPlanetsData()
         console.log(err);
         reject(err);
     })
-    .on('end',()=>{
-       
-        console.log(`${habitablePlanets.length} habitable planets found!!!`);
+    .on('end',async()=>{
+       const countPlanetFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetFound} habitable planets found!!!`);
         resolve();
     });
     });
@@ -48,9 +42,27 @@ function loadPlanetsData()
 
 async function getAllPlanets()
 {
-    return await planets.find({
-        keplerName
-    });
+    return await planets.find({});
+}
+
+
+async function savePlanet(planet)
+{
+    try{
+        await planets.updateOne({
+            keplerName:planet.kepler_name,
+        },{
+            keplerName:planet.kepler_name,
+        },{
+            upsert:true,
+        });
+    }catch(err)
+    {
+        console.error(`Could not save planet ${err}`);
+    }
+
+      
+         
 }
 
 
